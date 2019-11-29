3822ddf37233af63edf10bd06189dd03330d46eb
warngdan <warning_dan@hotmail.com>
2019-11-29 17:01:01 +0100

    Vrai première fois

---
+++ src/Bonjour.java
@@ -0,0 +1,20 @@
+
+/**
+ * Ce programme permet juste d'afficher Bonjour
+ * 
+ * @author Daniel
+ * @version 1.0
+ *
+ */
+public class Bonjour {
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Afficher Bonjour
+		System.out.println("Bonjour  ");
+	}
+
+}
