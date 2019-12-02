2ae44d2aded4ee1aff034ff7bd218d7e181798b7
warngdan <warning_dan@hotmail.com>
2019-12-02 09:59:25 +0100

    Rajout des bons fichiers

---
+++ WebContent/index.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="java.util.Date" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UT-8">
+<title>Démo JSP</title>
+</head>
+<body>
+	Bonjour, nous sommes <font color="red"><%=new Date ()%></font>
+</body>
+</html>
---
+++ src/com/gestionformation2/MaServlet2.java
@@ -0,0 +1,25 @@
+package com.gestionformation2;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/MaServlet2")
+public class MaServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+  
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("ICI DANS LA SERVLET");
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
